1f219ec270bce68389cc5202e2b444d15651d56c
storm-crypto <1742593818@qq.com>
2023-01-04 20:02:52 +0800

    json： 二维数组类型的json表示

--- test/testjson/testjson.cpp
+++ test/testjson/testjson.cpp
@@ -22,8 +22,26 @@ void func1()
     cout << sendBuf.c_str() << endl;
 }
 
+// 序列化示例2
+void func2()
+{
+    json js;
+
+    js["id"] = {1, 2, 3, 4, 5};
+    js["name"] = "zhang san";
+    // 添加对象
+    // js["msg"]["zhang san"] = "hello world";
+    // js["msg"]["liu shuo"] = "hello china";
+
+    // 跟上面的效果一样
+    js["msg"] = {{"zhang san", "hello world"}, {"liu shuo", "hello china"}};
+
+    cout << js << endl;
+}
+
 int main()
 {
-    func1();
+    // func1();
+    func2();
     return 0;
 }
