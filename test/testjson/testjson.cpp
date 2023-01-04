28155298c9ab9080565a879367a146417b077ff8
storm-crypto <1742593818@qq.com>
2023-01-04 20:11:39 +0800

    Json: 容器序列化

--- test/testjson/testjson.cpp
+++ test/testjson/testjson.cpp
@@ -26,8 +26,9 @@ void func1()
 void func2()
 {
     json js;
-
+    // 添加数组
     js["id"] = {1, 2, 3, 4, 5};
+    // 添加key-value
     js["name"] = "zhang san";
     // 添加对象
     // js["msg"]["zhang san"] = "hello world";
@@ -39,9 +40,34 @@ void func2()
     cout << js << endl;
 }
 
+// json序列化示例3
+void func3()
+{
+    json js;
+
+    // 直接序列化一个vector容器
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(5);
+
+    js["list"] = vec;
+
+    // 直接序列化一个map容器
+    map<int, string> m;
+    m.insert({1, "黄山"});
+    m.insert({2, "华山"});
+    m.insert({3, "泰山"});
+    js["path"] = m;
+
+    cout << js << endl;
+}
+
 int main()
 {
     // func1();
-    func2();
+    // func2();
+    func3();
+
     return 0;
 }
